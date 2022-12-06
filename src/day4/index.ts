de040e1590a241e2f8d116ec2bd1747284de7804
Ivan Ignatov <ivan@riparide.com>
2022-12-07 11:30:26 +1300

    Solution for day 4, part 1

---
+++ src/day4/index.ts
@@ -0,0 +1,41 @@
+import fs from 'fs'
+import path from 'path'
+
+const INPUT_PATH = './input.txt'
+
+const readInputFile = (relativePath: string = INPUT_PATH) => {
+  const buffer = fs.readFileSync(path.resolve(__dirname, relativePath))
+  return buffer.toString()
+}
+
+const parseByNewLine = (data: string) => data.split('\n')
+
+const parseRangeBounds = (rawRange: string): [number, number] => {
+  const [lowerBound, upperBound] = rawRange.split('-').map((value) => parseInt(value, 10))
+  return [lowerBound, upperBound]
+}
+
+const findOverlappingSections = () => {
+  const data = readInputFile()
+
+  const sectionRangePairs = parseByNewLine(data)
+
+  const overlappingSections = sectionRangePairs.filter((rangePair) => {
+    // parse range pairs
+    const [rawRange1, rawRange2] = rangePair.split(',')
+
+    const [range1LowerBound, range1UpperBound] = parseRangeBounds(rawRange1)
+    const [range2LowerBound, range2UpperBound] = parseRangeBounds(rawRange2)
+
+    // determine overlap (full)
+    const hasOverlap =
+      (range1LowerBound >= range2LowerBound && range1UpperBound <= range2UpperBound) ||
+      (range2LowerBound >= range1LowerBound && range2UpperBound <= range1UpperBound)
+
+    return hasOverlap
+  })
+
+  console.log('Number of assignment pairs with full overlap:', overlappingSections.length)
+}
+
+findOverlappingSections()
